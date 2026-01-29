da9e5abd3fb23a3090b79da905c0bfe78a5719d8
Bananin-DEV <mjvaldivieso6@utpl.edu.ec>
2026-01-29 14:38:31 -0500

    Ultimo ejercicio

--- ManejoArchivos/src/escritura05/Principal.java
+++ ManejoArchivos/src/escritura05/Principal.java
@@ -4,10 +4,45 @@
  */
 package escritura05;
 
-/**
- *
- * @author User
- */
+import java.util.Scanner;
+
 public class Principal {
-    
+
+    public static void main(String args[]) {
+
+        Scanner teclado = new Scanner(System.in);
+        boolean continuarIngreso = true;
+
+        while (continuarIngreso) {
+
+            System.out.println("Ingrese su nombre:");
+            String nombre = teclado.nextLine();
+
+            System.out.println("Ingrese su cédula:");
+            String cedula = teclado.nextLine();
+
+            System.out.println("Ingrese su placa:");
+            String placa = teclado.nextLine();
+            String inicialPlaca = placa.substring(0, 1);
+
+            System.out.println("Ingrese su marca:");
+            String marca = teclado.nextLine();
+
+            String registro = String.format(
+                    "%s, %s, %s, %s%n",
+                    nombre, cedula, placa, marca);
+
+            Procesos.DatoscrearArchivo(inicialPlaca, registro);
+
+            System.out.println("¿Desea ingresar más datos? (Si/No)");
+            String respuesta = teclado.nextLine();
+
+            if (respuesta.equalsIgnoreCase("No")) {
+                continuarIngreso = false;
+            }
+        }
+
+        teclado.close();
+    }
 }
+
--- ManejoArchivos/src/escritura05/Procesos.java
+++ ManejoArchivos/src/escritura05/Procesos.java
@@ -4,10 +4,47 @@
  */
 package escritura05;
 
-/**
- *
- * @author User
- */
+import java.util.Formatter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
 public class Procesos {
-    
+
+    public static void DatoscrearArchivo(String inicial, String mensaje) {
+
+        try {
+
+            if (inicial.equals("L")) {
+                Formatter salida = new Formatter(
+                        new OutputStreamWriter(
+                                new FileOutputStream("data/PlacaLoja.txt", true)));
+                salida.format("%s", mensaje);
+                salida.close();
+
+            } else if (inicial.equals("G")) {
+                Formatter salida = new Formatter(
+                        new OutputStreamWriter(
+                                new FileOutputStream("data/PlacaGuayas.txt", true)));
+                salida.format("%s", mensaje);
+                salida.close();
+
+            } else if (inicial.equals("P")) {
+                Formatter salida = new Formatter(
+                        new OutputStreamWriter(
+                                new FileOutputStream("data/PlacaPichincha.txt", true)));
+                salida.format("%s", mensaje);
+                salida.close();
+
+            } else {
+                Formatter salida = new Formatter(
+                        new OutputStreamWriter(
+                                new FileOutputStream("data/DatosSinCategoria.txt", true)));
+                salida.format("%s", mensaje);
+                salida.close();
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error al crear el archivo.");
+        }
+    }
 }
